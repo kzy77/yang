73751e4bf52caa175a07747d84761e55a3f31731
kzy77 <kzy77@qq.com>
2025-06-01 15:22:39 +0800

    Cloudflare pages部署警告消除

---
+++ next.config.ts
@@ -0,0 +1,14 @@
+import { NextConfig } from 'next';
+
+const nextConfig: NextConfig = {
+  reactStrictMode: true,
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  serverExternalPackages: ['next'],
+};
+
+export default nextConfig;
